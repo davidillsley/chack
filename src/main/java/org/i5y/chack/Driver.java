453ac2fbc6770fecb4cade3fd86af5ae08cce1a5
David Illsley <david@illsley.org>
2012-11-03 20:38:28 +0000

    Add twitter support

--- src/main/java/org/i5y/chack/Driver.java
+++ src/main/java/org/i5y/chack/Driver.java
@@ -17,10 +17,15 @@ import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-public class Driver {
-	
-	public static AtomicInteger amount = new AtomicInteger(0);
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
 
+public class Driver {
+
+	public static AtomicInteger amount = new AtomicInteger(0);
 	public static AtomicInteger itemsUsed = new AtomicInteger(0);
 
 	public static class DataSource extends HttpServlet {
@@ -31,8 +36,8 @@ public class Driver {
 			resp.setContentType("application/json");
 			resp.addHeader("Cache-Control", "no-store, max-age=0");
 			resp.getWriter().write("{");
-			resp.getWriter().write("\"amount\":"+amount.get()+",");
-			resp.getWriter().write("\"items_used\":"+itemsUsed.get());
+			resp.getWriter().write("\"amount\":" + amount.get() + ",");
+			resp.getWriter().write("\"items_used\":" + itemsUsed.get());
 			resp.getWriter().write("}");
 		}
 	}
@@ -42,27 +47,42 @@ public class Driver {
 		@Override
 		protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 				throws ServletException, IOException {
+			System.out.println("got a post!");
 			int increment = Integer.parseInt(req.getParameter("count"));
-			itemsUsed.addAndGet(increment);
+			if (itemsUsed.addAndGet(increment) > 2) {
+				TwitterFactory factory = new TwitterFactory();
+				Twitter twitter = factory.getInstance();
+				twitter.setOAuthConsumer(consumerKey, consumerSecret);
+				twitter.setOAuthAccessToken(new AccessToken(accessToken,
+						accessTokenSecret));
+				try {
+					Status status = twitter.updateStatus("We hit a limit");
+				} catch (TwitterException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
-	
+
 	public static class PaypalButton extends HttpServlet {
 
-		 final String FILE; 
-		
-		public PaypalButton() throws Exception{
-			BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("button.html")));
-			
+		final String FILE;
+
+		public PaypalButton() throws Exception {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					getClass().getClassLoader().getResourceAsStream(
+							"button.html")));
+
 			String line = br.readLine();
 			String file = "";
-			while(line != null){
-				file+=line;
+			while (line != null) {
+				file += line;
 				line = br.readLine();
 			}
 			FILE = file;
 		}
-		
+
 		@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 				throws ServletException, IOException {
@@ -77,28 +97,32 @@ public class Driver {
 		@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 				throws ServletException, IOException {
-			
+
 			String authToken = "ZjmIRL-WoHTIP6XvX2Ika6RUXAL3vkCjMCVYVPhI7VYaWaaF2P7CvEAB2WC";
 			String tx = req.getParameter("tx");
 			String amt = req.getParameter("amt");
-			amount.addAndGet((int)(Double.parseDouble(amt)*100));
+			amount.addAndGet((int) (Double.parseDouble(amt) * 100));
 			resp.getWriter().write("");
 		}
 	}
-	
+
+	static final String consumerKey = System.getenv("CONSUMER_KEY");
+	static final String consumerSecret = System.getenv("CONSUMER_SECRET");
+	static final String accessToken = System.getenv("ACCESS_TOKEN");
+	static final String accessTokenSecret = System
+			.getenv("ACCESS_TOKEN_SECRET");
+
 	public static void main(String[] args) throws Exception {
+
 		Server server = new Server(9777);
 
 		ServletContextHandler context = new ServletContextHandler(
 				ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
 
-		context.addServlet(new ServletHolder(new PaypalButton()),
-				"/button");
-		context.addServlet(new ServletHolder(new DataSource()),
-				"/data");
-		context.addServlet(new ServletHolder(new DataUpload()),
-				"/data-update");
+		context.addServlet(new ServletHolder(new PaypalButton()), "/button");
+		context.addServlet(new ServletHolder(new DataSource()), "/data");
+		context.addServlet(new ServletHolder(new DataUpload()), "/data-update");
 		context.addServlet(new ServletHolder(new PaypalCallback()),
 				"/paypalCallback");
 
